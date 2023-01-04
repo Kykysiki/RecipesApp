873ef1b2a30f592eac29091e05ccf405737e69b7
Глеб <bul.gleb@gmail.com>
2023-01-04 20:29:51 +0300

    Введение в Maven и сборку проектов

--- src/main/java/me/bulkanovga/recipesapp/controllers/FirstController.java
+++ src/main/java/me/bulkanovga/recipesapp/controllers/FirstController.java
@@ -1,2 +1,17 @@
-package me.bulkanovga.recipesapp.controllers;public class FirstController {
+package me.bulkanovga.recipesapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Приложение запущено";
+    }
+    @GetMapping("/info")
+    public String info() {
+        return "Имя ученика - Глеб, Название проекта - Рецепты, Дата создания - 04.01.2023, Описание проекта - в отдельном файле description.md.";
+    }
 }
